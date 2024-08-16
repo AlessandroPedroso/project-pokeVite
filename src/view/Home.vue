4c87ca8a9fdad915393b0540ff6c3f75e3cb93dc
Your Name <alessanrospedroso@outlook.com>
2024-08-16 08:48:27 -0300

    utilizando grid no template

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -2,7 +2,23 @@
 </script>
 
 <template>
-<h5>
-    Home
-</h5>
+    <main>
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col-sm-12 col-md-6">
+                    <div class="card" style="width: 18rem;">
+                        <img style="height: 200px; width: 200px;" src="https://www.ensino.eu/media/3kyh4vko/pokemon.jpg?width=300&height=300" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    Home1
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
