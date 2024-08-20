3d6a1881de0fe823a2860955aeed23b20cb2e1cf
Your Name <alessanrospedroso@outlook.com>
2024-08-20 09:03:43 -0300

    Criando lista de cards pokemon com bootstrap

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -3,6 +3,7 @@ import { onMounted,reactive,ref } from 'vue';
 import { api } from '../services/api.js'
 import ListPokemon from '../components/ListPokemon.vue'
 
+let urlBaseSvg = ref("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/")
 let pokemons = reactive(ref());
 
 async function getListPokemon() {
@@ -23,18 +24,21 @@ onMounted(() => {
         <div class="container">
             <div class="row mt-4">
                 <div class="col-sm-12 col-md-6">
-                    <div class="card" style="width: 18rem;">
+                    <!-- <div class="card" style="width: 18rem;">
                         <img style="height: 200px; width: 200px;" src="https://www.ensino.eu/media/3kyh4vko/pokemon.jpg?width=300&height=300" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk
                                 of the card's content.</p>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                 <div class="col-sm-12 col-md-6">
-                    <div class="card" style="width: 18rem;">
-                        <ListPokemon v-for="(pokemon,index) in pokemons" :key="index" :name="pokemon.name"/>
+                    <div class="card">
+                        <div class="card-body row">
+
+                            <ListPokemon v-for="(pokemon,index) in pokemons" :urlBaseSvg="urlBaseSvg + pokemon.url.split('/').at(6) + '.svg'" :key="index" :name="pokemon.name"/>
+                        </div>
                     </div>
                 </div>
             </div>
